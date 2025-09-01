4456429b6d1943f5994653608cada43182bf3927
dancerphil <dancerphil1994@gmail.com>
2025-09-01 20:43:02 +0800

    add editableMode

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,3 +1,4 @@
+/* --- group / ungroup --- */
 const getTabName = (hostname: string) => {
     const segment = hostname.split('.');
     for (let i = segment.length - 1; i >= 0; i--) {
@@ -49,11 +50,64 @@ const ungroup = async () => {
     chrome.tabs.ungroup(tabs.map(tab => tab.id));
 };
 
+/* --- editable --- */
+let editableMode = false;
+
+const toggleEditableMode = async () => {
+    chrome.tabs.query({active: true, currentWindow: true}, async (tabs) => {
+        const tab = tabs[0];
+        if (tab) {
+            editableMode = !editableMode;
+            await chrome.scripting.executeScript({
+                target: {tabId: tab.id},
+                func: (active: boolean) => {
+                    document.body.setAttribute('contenteditable', active.toString());
+
+                    const indicatorId = 'chrome-extension-editable-indicator';
+                    let indicatorElement = document.getElementById(indicatorId) as HTMLDivElement;
+
+                    if (active) {
+                        // 创建提示元素
+                        if (!indicatorElement) {
+                            indicatorElement = document.createElement('div');
+                            indicatorElement.id = indicatorId;
+
+                            // 设置样式
+                            Object.assign(indicatorElement.style, {
+                                position: 'fixed',
+                                top: 0,
+                                right: 0,
+                                bottom: 0,
+                                left: 0,
+                                borderRadius: '8px',
+                                zIndex: '2147483647',
+                                pointerEvents: 'none',
+                                border: '8px dashed red',
+                            });
+
+                            document.body.appendChild(indicatorElement);
+                        }
+                    }
+                    else {
+                        if (indicatorElement) {
+                            indicatorElement.remove();
+                        }
+                    }
+                },
+                args: [editableMode],
+            });
+        }
+    });
+};
+
 chrome.commands.onCommand.addListener((command) => {
     if (command === 'group') {
         group();
     }
     if (command === 'ungroup') {
         ungroup();
+    }
+    if (command === 'toggleEditableMode') {
+        toggleEditableMode();
     }
 });
